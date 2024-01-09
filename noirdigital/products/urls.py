ce739a0ffc1206d6d0680e3f6391577304e9c6d0
derek <dolson99@outlook.com>
2024-01-08 16:05:37 -0800

    added google fonts

--- noirdigital/products/urls.py
+++ noirdigital/products/urls.py
@@ -3,7 +3,7 @@ from .views import product_list
 
 app_name = 'products'
 urlpatterns = [
-    path("", product_list, name="product_list"),
+    path("product_list", product_list, name="product_list"),
     # ex: product/example
     # path("<int:product_id>/", views.detail, name="detail")
 ]
--- todo.md
+++ todo.md
@@ -4,3 +4,4 @@ make users app so they can submit photos, update photos, or delete photos
 work on visual look
 create black and white filter for user submitted photos
 find database appropriate for needs
+implement merriweather font for headings and paragraphs
